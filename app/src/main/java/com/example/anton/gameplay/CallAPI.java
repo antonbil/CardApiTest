14b862d10b61c77f4b889184b373df490a22367a
Anton <anton.bil@hz.nl>
2015-10-29 15:14:21 +0100

    first commit

---
+++ app/src/main/java/com/example/anton/gameplay/CallAPI.java
@@ -0,0 +1,159 @@
+package com.example.anton.gameplay;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
+
+/**
+ * makes http-requests
+ *
+ * Created by anton on 27-10-15.
+ */
+public class CallAPI extends AsyncTask<String, String, String> {
+    private static final char PARAMETER_DELIMITER = '&';
+    private static final char PARAMETER_EQUALS_CHAR = '=';
+
+    /**
+     *helper function to create string from key-value-pairs
+     * @param parameters pairwise description - value strings
+     * @return parameters converted to string
+     */
+    public static String createQueryStringForParameters(Map<String, String> parameters) {
+        StringBuilder parametersAsQueryString = new StringBuilder();
+        if (parameters != null) {
+            boolean firstParameter = true;
+
+            for (String parameterName : parameters.keySet()) {
+                if (!firstParameter) {
+                    parametersAsQueryString.append(PARAMETER_DELIMITER);
+                }
+
+                parametersAsQueryString.append(parameterName)
+                        .append(PARAMETER_EQUALS_CHAR)
+                        .append(URLEncoder.encode(
+                                parameters.get(parameterName)));
+
+                firstParameter = false;
+            }
+        }
+        return parametersAsQueryString.toString();
+    }
+
+    /**
+     *
+     * @param is stream to be changed
+     * @return stream converted to string
+     */
+    private static String convertStreamToString(InputStream is) {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+    private OnTaskCompleted listener;
+    private String call;
+
+    /**
+     * constructor for CallAPI
+     * @param listener = can be called as callback if result must be shown
+     * @param call=returned in callback so listener knows what call is processed
+     */
+    public CallAPI(OnTaskCompleted listener,String call){
+        this.listener=listener;
+        this.call=call;
+    }
+
+    /**
+     * do http-request in background
+     * can be get or post-request
+     * @param params
+     * @return
+     */
+    @Override
+    protected String doInBackground(String... params) {
+
+        String urlString=params[0]; // URL to call
+        String request=params[1];//get or post
+
+            String resultToDisplay = "";
+
+        InputStream in = null;
+
+        HttpURLConnection urlConnection;
+        Context context = (Context)this.listener;//context used to get string resources
+        // HTTP Get
+        try {
+
+            URL url = new URL(urlString);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            //http POST
+            if (request.equals(context.getString(R.string.POST))){
+                String postParameters=params[2];
+                urlConnection.setDoInput (true);
+                urlConnection.setDoOutput (true);
+                urlConnection.setUseCaches (false);
+                urlConnection.setRequestProperty("Host", "hz.nl");
+                urlConnection.setRequestMethod(context.getString(R.string.POST));
+                urlConnection.setFixedLengthStreamingMode(
+                        postParameters.getBytes().length);
+                try (PrintWriter out = new PrintWriter(urlConnection.getOutputStream())) {
+                    out.print(postParameters);
+                    Log.v("Post-parameters:",postParameters);
+                    out.close();
+                }
+            }//http POST
+
+
+        } catch (Exception e ) {
+
+            System.out.println(e.getMessage());
+
+            return e.getMessage();
+
+        }
+        try {
+            in = new BufferedInputStream(urlConnection.getInputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        resultToDisplay = convertStreamToString(in);
+        Log.v("Result in CallApi:",resultToDisplay);
+        return resultToDisplay;//resultToDisplay;
+    }
+
+    /**
+     * inform listener that request is finished
+     * @param result
+     */
+    protected void onPostExecute(String result) {
+        listener.onTaskCompleted(result,call);
+    }
+
+
+} // end CallAPI
---
+++ app/src/main/java/com/example/anton/gameplay/MainActivity.java
@@ -0,0 +1,213 @@
+package com.example.anton.gameplay;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends Activity  implements OnTaskCompleted{
+
+    public static final String CARD_API = "http://192.168.2.8/CardApi/";
+    //persistent data to store user-info, used to identify user
+    private String password;
+    private String name;
+    private String id;
+
+    final Context context = this;//to find resources etc
+    final OnTaskCompleted onTaskCompleted = this;//enables call-back methods
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //button save name
+        final Button savenamebutton = (Button) findViewById(R.id.button_savename);
+        savenamebutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Log.v("button", "save");
+                //save persistent data
+                EditText mEdit = (EditText) findViewById(R.id.id_text);
+                Log.v("EditText", mEdit.getText().toString());
+                id = ((EditText) findViewById(R.id.id_text)).getText().toString();
+                PreferenceManager.getDefaultSharedPreferences(context).edit().putString(getString(R.string.id_text), id).commit();
+                name = ((EditText) findViewById(R.id.name_text)).getText().toString();
+                PreferenceManager.getDefaultSharedPreferences(context).edit().putString(getString(R.string.name_text), name).commit();
+                password = ((EditText) findViewById(R.id.password_text)).getText().toString();
+                PreferenceManager.getDefaultSharedPreferences(context).edit().putString(getString(R.string.password_text), password).commit();
+            }
+        });
+        //button send player
+        final Button sendplayerbutton = (Button) findViewById(R.id.button_listgames);
+        sendplayerbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String urlString = CARD_API +String.format("games/%s/starting",id);
+                new CallAPI(onTaskCompleted, getString(R.string.startinggames)).execute(urlString, getString(R.string.GET));
+            }
+        });
+        //button_startgame
+        final Button startgamebutton = (Button) findViewById(R.id.button_startgame);
+        startgamebutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String gamenr = ((EditText) findViewById(R.id.id_gamenumber)).getText().toString();
+                String urlString = CARD_API +String.format("games/%s/%s/start",id,gamenr);
+                new CallAPI(onTaskCompleted, getString(R.string.startgame)).execute(urlString, getString(R.string.POST),
+                        CallAPI.createQueryStringForParameters(HashMapBuilder.build("password", password)));
+            }
+        });
+        //button list games
+        final Button listgamesbutton = (Button) findViewById(R.id.button_sendplayer);
+        listgamesbutton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String urlString = CARD_API +String.format("players/%s/add/%s",id,name);
+                new CallAPI(onTaskCompleted, getString(R.string.sendplayer)).execute(
+                        urlString,
+                        getString(R.string.POST),
+                        CallAPI.createQueryStringForParameters(HashMapBuilder.build("password", password)));
+              }
+        });
+        //get saved values for id,name and password
+        id = PreferenceManager.getDefaultSharedPreferences(context).getString(getString(R.string.id_text), "");
+        ((EditText) findViewById(R.id.id_text)).setText(id);
+        name = PreferenceManager.getDefaultSharedPreferences(context).getString(getString(R.string.name_text), "");
+        ((EditText) findViewById(R.id.name_text)).setText(name);
+        password = PreferenceManager.getDefaultSharedPreferences(context).getString(getString(R.string.password_text), "");
+        ((EditText) findViewById(R.id.password_text)).setText(password);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about_settings:
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                        context);
+
+                // set title
+                alertDialogBuilder.setTitle("About");
+
+                // set dialog message
+                alertDialogBuilder
+                        .setMessage("Test app created to demonstrate the use of CardApi\nBy Anton Bil")
+                        .setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // if this button is clicked, close
+                                // current dialog
+                                dialog.cancel();
+                            }
+                        });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+
+                // show it
+                alertDialog.show();
+                return true;
+        }
+        return false;
+    }    /**
+     * displays result of call to create new player
+     * @param result
+     */
+    private void sendPlayer(String result){
+        //display result in textview
+        TextView tView = (TextView) findViewById(R.id.sendplayerview);
+        tView.setText(result);
+    }
+
+    /**
+     * processes result of start-game request
+     * @param result
+     */
+    private void startGame(String result){
+        Log.v("display", result);//display in android logcat
+        try {
+            JSONObject jsonResultStartGameObject=new JSONObject(result);
+            String error=jsonResultStartGameObject.getString("error");
+            //display error message. Only reached when error part of json-message.
+            Toast.makeText(getApplicationContext(), "Error\n"+error, Toast.LENGTH_LONG).show();
+        } catch (JSONException e) {
+            //no error
+            //do whatever you must do with result if error not part of message
+        }
+        //display raw result in textview
+        TextView tView = (TextView) findViewById(R.id.startgameview);
+        tView.setText(result);
+    }
+    /**
+     * displays result of starting games
+     * @param result
+     */
+    private void startingGames(String result){
+        //example how to parse json-string in java
+
+        JSONArray jsonStartingGamesArray=null;
+        String id="";
+        String player="";
+        try {
+            jsonStartingGamesArray = new JSONArray(result);//resulting json-object is array in first place. First element of array is result.
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+        String s="";
+        for(int i = 0 ; i < jsonStartingGamesArray.length(); i++) {
+            try {
+                JSONObject jsonStartGameObject=jsonStartingGamesArray.getJSONObject(i);;
+                id = jsonStartGameObject.getString("id");
+                Log.v("display", id);//display in android logcat
+                player = jsonStartGameObject.getString("player");
+                s+=String.format(";id:%s,player:%s",id,player);
+            } catch (JSONException e) {
+                Log.e("JSON Parser", "Error parsing data " + e.toString());
+            }
+        }
+        //display processed result in textview
+        TextView tView = (TextView) findViewById(R.id.listgamesview);
+        tView.setText(s);
+
+    }
+
+    /**
+     * this method is called when get or post-requests are finished
+     * @param result = the result of the request, string in JSON-format
+     * @param call = the request-call that has finished
+     */
+    @Override
+    public void onTaskCompleted(String result, String call) {
+        if (call.equals(getString(R.string.startinggames))) startingGames(result);else
+        if (call.equals(getString(R.string.startgame))) startGame(result);else
+        if (call.equals(getString(R.string.sendplayer))) sendPlayer(result);
+
+    }
+
+}
+
